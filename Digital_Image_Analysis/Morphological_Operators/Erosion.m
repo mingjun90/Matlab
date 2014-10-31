bfa3b716ac882c8f287a670c00f9495b60c8c8ad
Lee <mingjun90@users.noreply.github.com>
2014-10-31 01:24:39 -0500

    NU_EECS_332_hw2_erosion

---
+++ Digital_Image_Analysis/Morphological_Operators/Erosion.m
@@ -0,0 +1,37 @@
+function img_out = Erosion(img_in, SE)
+%Erosion is used to shrink purticular part of an image
+%you can use the same idea on color images
+%
+%img_in is one layer gray(logical 0&1) data
+%SE is the structuring element, such as "9" , "+"
+%   "9"      %    "+"
+%   1 1 1    %   0 1 0
+%   1 1 1    %   1 1 1
+%   1 1 1    %   0 1 0
+
+[rows cols]=size(img_in);
+img_out=zeros(rows, cols);
+[r c]=size(SE);
+
+    for u = 1:rows
+        for v = 1:cols
+            if img_in(u,v)==1   %1 means white data
+                k=0;
+                
+                for  seu= round(-(r-1)/2):round((r-1)/2);
+                    for sev = round(-(c-1)/2):round((c-1)/2)
+                        if (0<u+seu && u+seu<=rows  && 0<v+sev && v+sev<=cols && SE(seu+round(r/2),(sev+round(c/2)))<=img_in(u+seu,v+sev))
+                            k=k+1;  %count the matching point of SE
+                        end 
+                    end
+                end
+                
+                %if SE matches, keep the center point
+                if k==r*c
+                    img_out(u,v)=1;
+                end
+            end
+        end
+    end
+
+end
